f4d535d8d77ccdb095791bb464cd1f574be50d2d
Elijah Soria <elijahsoria@google.com>
2022-11-06 19:36:26 -0800

    Add genre sort

--- movies.js
+++ movies.js
@@ -1,29 +1,47 @@
 let mvlist = [];
 let mvlist_recently_watched = [];
+let mvlist_genre = {};
+
+function createMovieRow(mv) {
+	let name = document.createElement("div");
+	name.innerHTML = mv.title;
+	name.classList.add("c");
+	let rtg = document.createElement("div"); 
+	rtg.innerHTML = mv.rating;
+	rtg.classList.add("c");
+	let mvDiv = document.createElement("div");
+	mvDiv.classList.add("r");
+	mvDiv.appendChild(name);
+	mvDiv.appendChild(rtg);
+	let sub = document.createElement("div");
+	sub.innerHTML = "(" + mv.year + ") - " + mv.genres.join(", ");
+	sub.classList.add("sub");
+	let wrapDiv = document.createElement("div");
+	wrapDiv.appendChild(mvDiv);
+	wrapDiv.appendChild(sub);
+	return wrapDiv;
+}
 
 function updateMovieEl(mvs) {
 	let listdiv = document.getElementById('movie-list');
 	listdiv.innerHTML = "";
 	for (let i = 0; i < mvs.length; i++) {
-			let mv = mvs[i];
-			let name = document.createElement("div");
-			name.innerHTML = mv.title;
-			name.classList.add("c");
-			let rtg = document.createElement("div"); 
-			rtg.innerHTML = mv.rating;
-			rtg.classList.add("c");
-			let mvDiv = document.createElement("div");
-			mvDiv.classList.add("r");
-			mvDiv.appendChild(name);
-			mvDiv.appendChild(rtg);
-			let sub = document.createElement("div");
-			sub.innerHTML = "(" + mv.year + ") - " + mv.genres.join(", ");
-			sub.classList.add("sub");
-			let wrapDiv = document.createElement("div");
-			wrapDiv.appendChild(mvDiv);
-			wrapDiv.appendChild(sub);
-			listdiv.appendChild(wrapDiv); 
-    		}
+		listdiv.appendChild(createMovieRow(mvs[i]));
+	}
+};
+
+function updateMovieElWithGenre() {
+	let listdiv = document.getElementById('movie-list');
+	listdiv.innerHTML = "";
+	for (const key in mvlist_genre.keys().sort()) {
+		let gen = document.createElement("div");
+		gen.classList.add("gen");
+		gen.innerHTML = "-- " + key + " --";
+		listdiv.appendChild(gen);
+		for (const mv in mvlist_genre[key]) {
+			listdiv.appendChild(createMovieRow(mv));
+		}
+	}
 };
 
 function createMovieList() {
@@ -59,28 +77,40 @@ function stringSortFn(a,b) {
 		    }
 };
 
+function sortMovieFn(a, b) {
+	if (a.rating == b.rating) {
+		return stringSortFn(a.title, b.title);
+	} else {
+		return parseFloat(b.rating) - parseFloat(a.rating);
+	}
+}
+
 function sortMovies(event) {
   console.log("updating...");
   let menu = document.getElementById("sort_by");
   if (menu.value == 'title') {
     updateMovieEl(mvlist.sort((a,b) => stringSortFn(a.title, b.title)));
   } else if (menu.value == 'high_rated') {
-    updateMovieEl(mvlist.sort((a,b) => {
-	    if (a.rating == b.rating) {
-		    return stringSortFn(a.title, b.title);
-	    } else {
-		    return parseFloat(b.rating) - parseFloat(a.rating);
-	    }
-    }));
+    updateMovieEl(mvlist.sort(sortMovieFn));
   } else if (menu.value == 'low_rated') {
-    updateMovieEl(mvlist.sort((a,b) => {
-	    if (a.rating == b.rating) {
-		    return stringSortFn(a.title, b.title);
-	    } else {
-		    return parseFloat(a.rating) - parseFloat(b.rating);
-	    }
-    }));
+    updateMovieEl(mvlist.sort(sortMovieFn));
   } else if (menu.value == 'recently_watched') {
     updateMovieEl(mvlist_recently_watched);
-  } 
+  } else if (menu.value == 'genre') {
+	  if (Object.keys(mvlist_genre).length === 0) {
+		  for (const mv in mvlist) {
+			  for (const gen in mv.genres) {
+				  if (gen in mvlist_genre) {
+					  mvlist[gen].push(mv);
+				  } else {
+					  mvlist_genre[gen] = [mv];
+				  }
+			  }
+		  }
+		  for (const key in mvlist_genre.keys()) {
+			  mvlist_genre[key].sort(sortMovieFn);
+		  }
+	  }
+	  updateMovieElWithGenre();
+  }
 };
