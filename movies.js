c42b2d9375ed2e44df447095046d8808c1d627dc
Elijah Soria <elijahsoria@google.com>
2022-11-06 18:24:55 -0800

    Use object

--- movies.js
+++ movies.js
@@ -9,10 +9,10 @@ function updateMovieEl(mvs) {
 			let mvDiv = document.createElement("div");
 			mvDiv.classList.add("r");
 			let name = document.createElement("div");
-			name.innerHTML = mv[0];
+			name.innerHTML = mv.title;
 			name.classList.add("c");
 			let rtg = document.createElement("div"); 
-			rtg.innerHTML = mv[1];
+			rtg.innerHTML = mv.rating;
 			rtg.classList.add("c");
 			mvDiv.appendChild(name);
 			mvDiv.appendChild(rtg);
@@ -27,8 +27,13 @@ function createMovieList() {
     		let mvs = text.split("\n");
     		for (let i = mvs.length - 2; i >= 0; i--) {
 			let mv = mvs[i].split(";");
-			if (mv[0] && mv[1]) {
-				mvlist.push([mv[0], mv[1]]);
+			if (mv[0] && mv[1] && mv[2] && mv[3]) {
+				mvlist.push({
+					title: mv[0],
+					rating: mv[1],
+					year: mv[2],
+					genres: mv[3].split(",") 
+				});
 			}
     		}
 	        mvlist_recently_watched = [...mvlist];
@@ -52,21 +57,21 @@ function sortMovies(event) {
   console.log("updating...");
   let menu = document.getElementById("sort_by");
   if (menu.value == 'title') {
-    updateMovieEl(mvlist.sort((a,b) => stringSortFn(a[0], b[0])));
+    updateMovieEl(mvlist.sort((a,b) => stringSortFn(a.title, b.title)));
   } else if (menu.value == 'high_rated') {
     updateMovieEl(mvlist.sort((a,b) => {
-	    if (a[1] == b[1]) {
-		    return stringSortFn(a[0], b[0]);
+	    if (a.rating == b.rating) {
+		    return stringSortFn(a.title, b.title);
 	    } else {
-		    return parseFloat(b[1]) - parseFloat(a[1]);
+		    return parseFloat(b.rating) - parseFloat(a.rating);
 	    }
     }));
   } else if (menu.value == 'low_rated') {
     updateMovieEl(mvlist.sort((a,b) => {
-	    if (a[1] == b[1]) {
-		    return stringSortFn(a[0], b[0]);
+	    if (a.rating == b.rating) {
+		    return stringSortFn(a.title, b.title);
 	    } else {
-		    return parseFloat(a[1]) - parseFloat(b[1]);
+		    return parseFloat(a.rating) - parseFloat(b.rating);
 	    }
     }));
   } else if (menu.value == 'recently_watched') {
